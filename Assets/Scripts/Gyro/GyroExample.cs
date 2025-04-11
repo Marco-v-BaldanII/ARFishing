5129a4a31e732d530b295bbc8c50f2a2a14a5d03
VM Marco <59030832+Marco-v-BaldanII@users.noreply.github.com>
2025-04-11 11:31:48 +0200

    gyroTesting

--- Assets/Scripts/Gyro/GyroExample.cs
+++ Assets/Scripts/Gyro/GyroExample.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
 
-public class GyroExample : MonoBehaviour
+public class GyroManager : MonoBehaviour
 {
     private Gyroscope gyro;
 
+    public static GyroManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+
+        }
+    }
+
+    public Material mat;
+
     void Start()
     {
+        gyro = Input.gyro;
+        gyro.enabled = true;
         if (SystemInfo.supportsGyroscope)
         {
             gyro = Input.gyro;
             gyro.enabled = true;
+            mat.color = Color.blue;
         }
         else
         {
             print("Gyroscope not supported on this device.");
+            mat.color = Color.red;
         }
     }
 
@@ -21,6 +37,7 @@ public class GyroExample : MonoBehaviour
     {
         if (gyro != null)
         {
+            mat.color = Color.green;
             Quaternion deviceRotation = gyro.attitude;
             transform.rotation = deviceRotation;
         }
